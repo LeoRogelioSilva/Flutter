a26eae532151342eaaa4774f0fd3600e90059284
LeoRogelioSilva <l177346@dac.unicamp.br>
2022-09-15 15:41:14 -0300

    wip

--- atividade2/lib/screens/formulario.dart
+++ atividade2/lib/screens/formulario.dart
@@ -1,6 +1,8 @@
 import 'package:e_forms_intro/model/login_data.dart';
 import 'package:flutter/material.dart';
 import 'package:e_forms_intro/model/user.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 class CompleteForm extends StatefulWidget {
   const CompleteForm({super.key});
@@ -15,72 +17,100 @@ class CompleteFormState extends State<CompleteForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
+    return Container(
       key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Login',
-            ),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Insira seu login';
-              }
-              return null;
-            },
-            onSaved: (value) => _newUser.nome = value,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Email',
-            ),
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Insira seu email';
-              }
-              return null;
-            },
-            onSaved: (value) => _newUser.email = value,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Senha',
-            ),
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-            onSaved: (value) => _newUser.senha = value,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  _formKey.currentState?.save();
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      backgroundColor: Colors.green,
-                      duration: const Duration(seconds: 5),
-                      content: Text("Bem Vindo ${_newUser.toString()}!"),
-                      action: SnackBarAction(label: "Home", onPressed: () {})));
-                }
-              },
-              child: const Text('Enviar'),
-            ),
-          ),
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[300]!,
+            blurRadius: 6,
+            spreadRadius: 2,
+            offset: const Offset(0.2, 0.3),
+          )
         ],
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.grey[200],
+      ),
+      child: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Login',
+                focusColor: Colors.white,
+                //add prefix icon
+                prefixIcon: Icon(
+                  Icons.person_outline_rounded,
+                  color: Colors.grey,
+                ),
+                fillColor: Colors.grey,
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontFamily: "verdana_regular",
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Insira seu login';
+                }
+                return null;
+              },
+              onSaved: (value) => _newUser.nome = value,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Email',
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Insira seu email';
+                }
+                return null;
+              },
+              onSaved: (value) => _newUser.email = value,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Senha',
+              ),
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (value) => _newUser.senha = value,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState?.save();
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        backgroundColor: Colors.green,
+                        duration: const Duration(seconds: 5),
+                        content: Text("Bem Vindo ${_newUser.toString()}!"),
+                        action:
+                            SnackBarAction(label: "Home", onPressed: () {})));
+                  }
+                },
+                child: const Text('Enviar'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
-  }
-
-  String getUser() {
-    return _newUser.toString();
   }
 }
