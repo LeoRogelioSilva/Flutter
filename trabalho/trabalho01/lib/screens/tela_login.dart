38343fbfb8b98e536671129b05fb8b8556f36e65
LeooRS <l177346@dac.unicamp.br>
2022-09-28 20:45:30 -0300

    wip

--- trabalho/trabalho01/lib/screens/tela_login.dart
+++ trabalho/trabalho01/lib/screens/tela_login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:e_forms_intro/model/user.dart';
+import 'package:trabalho01/model/user.dart';
 import 'package:email_validator/email_validator.dart';
 
 class LoginScreen extends StatefulWidget {
